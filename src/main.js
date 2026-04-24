4b7aa064e05bebaffa76f6faf5abdd9ae1249892
Pierre <pierredltre@icloud.com>
2026-04-24 19:37:13 +0200

    smooth scroll retiré

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import Lenis from '@studio-freight/lenis'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import App from './App.vue'
@@ -9,43 +8,7 @@ import { initializeLocale } from '@/i18n'
 
 import './styles/main.css'
 
-const lenis = new Lenis({
-  duration: 1.4,
-  smooth: true,
-  smoothTouch: true,
-  lerp: 0.12,
-  easing: (t) => t
-})
-
-if (typeof window !== 'undefined') {
-  window.__lenis = lenis
-}
-
 gsap.registerPlugin(ScrollTrigger)
-
-ScrollTrigger.scrollerProxy(document.body, {
-  scrollTop(value) {
-    if (typeof value === 'number') {
-      lenis.scrollTo(value, { immediate: true })
-    } else {
-      return window.scrollY || document.documentElement.scrollTop || 0
-    }
-  },
-  getBoundingClientRect() {
-    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
-  }
-})
-
-lenis.on('scroll', () => {
-  ScrollTrigger.update()
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
 
 initPageAnimations()
 initializeLocale()
